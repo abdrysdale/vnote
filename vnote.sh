e23d527dbaf451dfd22f25175721cf65395deb48
Alex <adrysdale@protonmail.com>
2021-03-30 14:12:46 +0100

    Updated vnote to include tag listing and searching.

--- vnote.sh
+++ vnote.sh
@@ -1,73 +1,45 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 # Defines default notes settings
-dir="${HOME}/Dropbox/Notes/"
+dir="${HOME}/PhD/Notes/"
 ext='.md'
-default='notes.md'
-
-# Colours
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-# Line parameters
-COLS=$(tput cols)
-CENT_COL=$((COLS/2))
-SEP="-"
-MSG="Notes" 
+default='index.md'
 
 # Defaults to default file with no argument
 if [ $# -eq 0 ]
 then
 
-	vim "${dir}${default}"
+	filename="${default}"
 
 else
 
-	# Checks if an extension was passed and defaults to markdown
-	if [[ ${1} =~ "." ]]
-	then
+	filename="${1}"
 
-		filename="$1"
-	else
-		filename="$1${ext}"
-	fi
-
-	# Checks if a directory was passed
-	if [ $# -gt 1 ]
-	then
-		MSG=$2
-		dir="${dir}$2/"
-
-		# Creates a directory if the passed directory doesn't exist
-		if [ ! -d ${dir} ]; then
-			mkdir $dir
-		fi
-	fi
-	
-	# Can pass ls as file name to list notes dir
-	if [ ${filename} = 'ls'${ext} ]
+	# Can pass --ls or -l as file name to list tags
+	if [ ${filename} = '--ls' ] || [ ${filename} = '-l' ]
 	then
 		
-		# Centeres message
-		MSG_LEN=${#MSG}
-		MSG_COL=$((CENT_COL - MSG_LEN/2))
+		# Lists tags
+		grep -ho @[a-z]* ${dir}*${ext} | sort | uniq -c | sort -nr
 
-		# Prints message to screen
-		printf "${GREEN}"
-		for i in $(seq 1 $MSG_COL)
-		do
-			printf "${SEP}"
-		done
-		printf "${MSG}"
-		for i in $(seq 1 $MSG_COL)
-		do
-			printf "${SEP}"
-		done
-		printf "${NC}\n"
+	# Can pass --search-tag or -s to search by tags to edit a file.
+	elif [ ${filename} = '--search-tag' ] || [ ${filename} = '-s' ]
+	then
 
-		# Lists dir
-		ls --color=auto ${dir}
+		filename=$(grep -Ho @[a-z]* ${dir}*${ext} | rev | cut -d'/' -f 1 | rev | fzf | cut -d : -f 1)
+		vim ${dir}${filename}
+
 	else
+
+		# Checks if an extension was passed and defaults to markdown
+		if [[ ${1} =~ "." ]]
+		then
+
+			filename="$1"
+		else
+			filename="$1${ext}"
+		fi
+	
 		# Creates the note in the folder
 		vim "${dir}${filename}"
 	fi 
